a469c80b92ea90726c4986819f383134717f1842
lionelus <lionelus@live.fr>
2017-06-02 11:12:42 +0200

    Update extract_one_fast5_file_from_bz2_file.sh

--- extract_one_fast5_file_from_bz2_file.sh
+++ extract_one_fast5_file_from_bz2_file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-f=/fast5.directory/$1.tar.bz2 ; tar -xf $f --occurrence $(tar -tf $f | grep -e '\.fast5$' | head -n 1) -O > fast5_file.fast5 
+f=/import/rhodos07/nanopore/raw/$1.tar.bz2 ; tar -xf $f --occurrence $(tar -tf $f | grep -e '\.fast5$' | head -n 1) -O > fast5_file.fast5 
