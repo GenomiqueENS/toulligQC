ec7fe6ac14b3721061d3bda301063b5be2373a54
lionelus <lionelus@live.fr>
2017-06-02 16:03:08 +0200

    Update extract_one_fast5_file_from_bz2_file.sh

--- extract_one_fast5_file_from_bz2_file.sh
+++ extract_one_fast5_file_from_bz2_file.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-f=/fast5.directory/$1.tar.bz2 ; tar -xf $f --occurrence $(tar -tf $f | grep -e '\.fast5$' | head -n 1) -O > /design_file_directory/fast5_file.fast5 
+f=/fast5.directory/$1.tar.bz2 ; tar -xf $f --occurrence $(tar -tf $f | grep -e '\.fast5$' | head -n 1) -O > /design.file.directory/fast5_file.fast5 
