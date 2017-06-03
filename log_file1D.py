d84316236f83d4c5865ebe552ef26b144de3eea1
lionelus <lionelus@live.fr>
2017-06-03 10:55:23 +0200

    Update log_file1D.py

--- log_file1D.py
+++ log_file1D.py
@@ -15,8 +15,10 @@ def log_file1D(fast5_data , basecall_stat):
     counter_template, total_nucleotide_template = basecall_stat.counter()
 
     occupancy_pore = basecall_stat.occupancy_pore()
-
-    completeName = os.path.join('statistics/', "run_statistics_file.txt")
+    if os.path.isfile('/configpass/docker_config.txt'):
+		completeName = os.path.join('/working.directory/statistics/', "run_statistics_file.txt")
+    else:
+		completeName = os.path.join('statistics/', "run_statistics_file.txt")
 
     with open(completeName, 'w') as file_data:
 
@@ -48,4 +50,3 @@ def log_file1D(fast5_data , basecall_stat):
 
         for index, element in basecall_stat.statistics_read_size().iteritems():
             file_data.write("Read.fastq.length.{}={}\n".format(index, element))
-
