fe8c4e94ca3f55e06ae6c3fc184cb09ae7229b8b
Lionel Ferrato <ferrato@fulla>
2017-07-10 18:19:11 +0200

    read_file.sh modification

--- read_file.sh
+++ read_file.sh
@@ -1,11 +1,23 @@
-config_file=$HOME/Documents/toulligQC-master/config.txt
-docker_config_file=$HOME/Documents/toulligQC-master/docker_config.txt
+config_file=/import/pontos06/analyses/toulligQC/config.txt
+docker_config_file=/import/pontos06/analyses/toulligQC/config.txt
  
 index=0
-for line in $(cut -f2 -d = "${config_file}"); 
+
+for line in $(cut -f2 -d = "${config_file}");
+
 do
-	path[index]=$(readlink -f "${line}")
-	index=$(expr $index + 1)
+  if [ $index -eq 6 ] || [ $index -eq 7 ] || [ $index -eq 8 ]; then
+    path[index]=$line
+    index=$(expr $index + 1)
+
+  elif [[ $line =~ config ]];then
+    path[index]=$line
+    index=$(expr $index + 1)
+
+  else
+    path[index]=$(readlink -f "${line}")
+    index=$(expr $index + 1)
+  fi
 done
 
 mapfile -t config_name_var < <( cut -f1 -d = ${config_file} )
@@ -18,16 +30,18 @@ if test -f "${docker_config_file}"
 
 for line in ${config_name_var[@]};
 do
-	if [[ $line =~ config ]]; then
-		echo -e [config] >> "${docker_config_file}"
-		i=$(expr $i+1)
-	
-	else
-		echo -e "${config_name_var[i]}=${path[i]}/" >> "${docker_config_file}"
-
-		i=$(expr $i+1)
-	fi
+  if [[ $line =~ config ]]; then
+    echo -e [config] >> "${docker_config_file}"
+    i=$(expr $i+1)
+# echo $line
+ elif [[ $line =~ \[extension\] ]]; then
+   echo -e [extension] >> "${docker_config_file}"
+  i=$(expr $i+1)
+  else
+    echo -e "${config_name_var[i]}=${path[i]}" >> "${docker_config_file}"
+    i=$(expr $i+1)
+  fi
 done
 
+docker run -ti --rm -v ${path[1]}:${path[1]} -v ${path[2]}:${path[2]} -v ${path[3]}:${path[3]} -v ${path[4]}:${path[4]} -v $(pwd):$(pwd) -v ${path[5]}:${path[5]} -u $(id -u):$(id -g) genomicpariscentre/toulligqc 
 
-docker run -ti --rm -v ${path[1]}:${path[1]} -v ${path[2]}:${path[2]} -v ${path[3]}:${path[3]} -v ${path[4]}:${path[4]} -v ${path[5]}:${path[5]} genomicpariscentre/toulligqc
