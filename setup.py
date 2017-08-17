7d1533435647194800891bd0332531995ee40384
Lionel Ferrato <ferrato@fulla>
2017-08-17 10:52:11 +0200

    setup modification

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-"""A setuptools based setup module.
+import os
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+version_py = os.path.join(os.path.dirname(__file__), 'poretools', 'version.py')
+version = open(version_py).read().strip().split('=')[-1].replace('"', '')\
+    .strip()
+
+"""A setuptools based setup module.i
+
 See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
 
 here = path.abspath(path.dirname(__file__))
 
@@ -59,7 +64,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['toulligqc'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
@@ -69,30 +74,15 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    python_requires='>=3' 
+    python_requires='>=3',
     install_requires=['matplotlib', 'seaborn', 'h5py', 'pandas', 'numpy'],
-    
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
+
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'sample': ['package_data.dat'],
-    },
+    #package_data={'sample': ['package_data.dat']},
 
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
