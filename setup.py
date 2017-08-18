5473f6728f5acc2cbe62039409d62dcc3a973fb1
Lionel Ferrato <ferrato@fulla>
2017-08-18 14:18:45 +0200

    comments deletion in setup.py

--- setup.py
+++ setup.py
@@ -24,9 +24,6 @@ print(version)
 setup(
     name='toulligqc',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version=version,
 
     description='A data analysis tool of MinION runs',
@@ -39,60 +36,33 @@ setup(
     author='Lionel Ferrato-Berberian',
     author_email='ferrato@biologie-ens.fr',
 
-    # Choose your license
     license='GPL V3',
     plateformes = 'ALL',
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         'Development Status :: 3 - Alpha',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
 
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: GPL V3',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
 
-    # What does your project relate to?
     keywords='MinION analysis report',
 
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
     packages=['toulligqc'],
-#find_packages(exclude=['docs', 'test_data']),
     package_dir={'toulligqc': "toulligqc"},
     package_data={'toulligqc': []},
     zip_safe=False,
     include_package_data=True,
 
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     python_requires='>=3',
-    install_requires=['matplotlib==2.0.0', 'seaborn==0.7.1', 'h5py==2.6.0', 'pandas==0.19.2', 'numpy==1.11.3'],
+    install_requires=['matplotlib==2.0.0', 'seaborn==0.7.1', 'h5py==2.6.0', \
+                      'pandas==0.19.2', 'numpy==1.11.3'],
 
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    #package_data={'sample': ['package_data.dat']},
-
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'toulligqc=toulligqc.toulligqc_main:main',
