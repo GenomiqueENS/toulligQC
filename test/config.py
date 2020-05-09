d5ac4f355e85997055bccdd789ea5e91059beae0
Karine Dias <dias@bio.ens.psl.eu>
2020-05-09 17:50:13 +0200

    Create unit test configuration file

---
+++ test/config.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+###Configuration file for all unit tests###
+
+whole_config= {
+"barcoding": "True",
+"result_directory": "/home/karine/src/python/toulligqc2/data/output/",
+"dpi": "100",
+"sequencing_summary_source": "/home/karine/src/python/toulligqc2/data/sequencing_summary_small.txt\t/home/karine/src/python/toulligqc2/data/barcoding_summ_pass_small.txt\t/home/karine/src/python/toulligqc2/data/barcoding_summ_fail_small.txt"
+}
+
+
+no_seq_summary_source_config= {
+"barcoding": "True",
+"result_directory": "/home/karine/src/python/toulligqc2/data/output/",
+"dpi": "100",
+"sequencing_summary_source": "\t"
+}
+
+directory_config= {
+"barcoding": "True",
+"result_directory": "/home/karine/src/python/toulligqc2/data/output/",
+"dpi": "100",
+"sequencing_summary_source": "/home/karine/src/python/toulligqc2/data/"
+}
+
+only_seq_summary_config= {
+"barcoding": "True",
+"result_directory": "/home/karine/src/python/toulligqc2/data/output/",
+"dpi": "100",
+"sequencing_summary_source": "/home/karine/src/python/toulligqc2/data/sequencing_summary_small.txt"
+}
+
+only_barcoding_config= {
+"barcoding": "True",
+"result_directory": "/home/karine/src/python/toulligqc2/data/output/",
+"dpi": "100",
+"sequencing_summary_source": "/home/karine/src/python/toulligqc2/data/barcoding_summ_pass_small.txt\t/home/karine/src/python/toulligqc2/data/barcoding_summ_fail_small.txt"
+}
+
+random_file_config= {
+"barcoding": "True",
+"result_directory": "/home/karine/src/python/toulligqc2/data/output/",
+"dpi": "100",
+"sequencing_summary_source": "/home/karine/src/python/toulligqc2/data/test.txt"
+}
+
