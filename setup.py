a3ec265d0f0f4bc50637ded7c08855746592e504
Karine <dias@bio.ens.psl.eu>
2020-11-16 17:59:39 +0100

    Update Python version in pypi classifier

--- setup.py
+++ setup.py
@@ -34,9 +34,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
 
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8'
     ],
 
     keywords='Nanopore MinION QC report',
