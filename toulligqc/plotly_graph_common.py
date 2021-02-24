3bac7e8ba834a1adf1f3d6a98b4d48b4ed6a4044
Laurent Jourdren <jourdren@biologie.ens.fr>
2021-02-24 12:40:21 +0100

    UPDATE: Add x-axis and y-axis title for barcode boxplot graphs.

--- toulligqc/plotly_graph_common.py
+++ toulligqc/plotly_graph_common.py
@@ -548,8 +548,8 @@ def _pie_chart_graph(graph_name, count_sorted, color_palette, one_d_square, resu
                     ),
                     dict(
                         args=[{'visible': [False, True]},
-                              {'xaxis': {'visible': True},
-                               'yaxis': {'visible': True},
+                              {**_xaxis('Barcodes', dict(visible=True)),
+                               **_yaxis('Read count', dict(visible=True)),
                                'plot_bgcolor': plotly_background_color}],
                         label="Histogram",
                         method="update"
