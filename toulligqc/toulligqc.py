7a5a58c7c649bdeec69345721d2bc33fc72a6c47
Hunter Cameron <hunter.cameron418@gmail.com>
2021-08-25 23:21:54 -0400

    sync group pull with new regex

--- toulligqc/toulligqc.py
+++ toulligqc/toulligqc.py
@@ -313,7 +313,7 @@ def main():
             for b in config_dictionary['barcodes'].strip().split(','):
                 pattern = re.search(r'(BC|RB|NB|BARCODE)(\d{2})', b.strip().upper())
                 if pattern:
-                    barcode = 'barcode{}'.format(pattern.group(1))
+                    barcode = 'barcode{}'.format(pattern.group(2))
                     barcode_set.add(barcode)
             barcode_selection = sorted(barcode_set)
 
