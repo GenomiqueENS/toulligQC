d46fcfd1c61a005e860507a17d922a7aa77adf23
Laurent Jourdren <jourdren@biologie.ens.fr>
2024-06-11 11:13:48 +0200

    UPDATE: Update dependencies and version dependencies in setup.py and Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,23 @@
-FROM ubuntu:23.10
+FROM ubuntu:24.04
 
 MAINTAINER Laurent Jourdren <jourdren@bio.ens.psl.eu>
-ARG VERSION=2.5.7
+ARG VERSION=2.7.0
 ARG INSTALL_PACKAGES="git"
 RUN apt update && \
     DEBIAN_FRONTEND=noninteractive apt install --yes \
                     $INSTALL_PACKAGES \
                     python3 \
                     python3-pip\
-                    python3-tk\
-                    python3-h5py\
                     python3-matplotlib\
-                    python3-scipy\
+                    python3-plotly\
+                    python3-h5py\
                     python3-pandas\
                     python3-numpy\
+                    python3-scipy\
+                    python3-sklearn \
                     python3-tqdm\
-                    python3-sklearn && \
-    pip3 install --break-system-packages "ezcharts==0.7.6" "plotly==4.5.0" && \
+                    python3-pysam && \
+    pip3 install --break-system-packages "pod5==0.3.10" "ezcharts==0.7.6" && \
     cd /tmp && \
     git clone https://github.com/GenomicParisCentre/toulligQC && \
     cd toulligQC && \
--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setup(
     long_description='See project website for more information.',
 
     # The project's main homepage.
-    url='https://github.com/GenomicParisCentre/toulligQC',
+    url='https://github.com/GenomiqueENS/toulligQC',
 
     # Author details
     author='Genomic Paris Centre team',
-    author_email='toulligqc@biologie.ens.fr',
+    author_email='toulligqc@bio.ens.psl.eu',
 
     license='GPL V3',
     platforms='ALL',
@@ -34,7 +34,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
 
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.12'
     ],
 
     keywords='Nanopore MinION QC report',
@@ -46,10 +46,10 @@ setup(
     include_package_data=True,
 
     python_requires='>=3.11.0',
-    install_requires=['matplotlib>=3.6.3',   'plotly==4.5.0', 'h5py>=3.7.0',
-                      'pandas>=1.5.3',       'numpy>=1.24.2',  'scipy>=1.10.1',
-                      'scikit-learn>=1.2.1', 'tqdm>=4.64.1',   'pysam>=0.21.0',
-                      'ezcharts==0.7.6'],
+    install_requires=['matplotlib>=3.6.3',   'plotly==5.15.0', 'h5py>=3.10.0',
+                      'pandas>=2.1.4',       'numpy>=1.26.4',  'scipy>=1.11.4',
+                      'scikit-learn>=1.4.1', 'tqdm>=4.66.2',   'pysam>=0.22.0',
+                      'pod5>=0.3.10' 'ezcharts==0.7.6'],
 
     entry_points={
         'console_scripts': [
